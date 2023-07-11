656e9e224599c9ebf5cc546dc60420bd9c96731d
mrajter <miroslav.rajter@gmail.com>
2023-07-11 08:43:04 +0200

    samll error in comp.flex

--- R/comperimeter.R
+++ R/comperimeter.R
@@ -240,7 +240,7 @@ comperimeter <- function(data, form, by = NA, by_total = TRUE, param = TRUE, m.d
         if (r.names$N[i] == 0) {
           fre.t <- c(fre.t, NA)
         } else {
-          fre.t <- c(fre.t, length(data[[v.names[i]]][data[[v.names[i]]] == v.labs$vals[j] & is.na(data[[v.names[1]]])==FALSE]) * 100 / r.names$N[i])
+          fre.t <- c(fre.t, length(data[[v.names[i]]][data[[v.names[i]]] == v.labs$vals[j] & is.na(data[[v.names[i]]])==FALSE]) * 100 / r.names$N[i])
         }
       }
       r.fre <- rbind(r.fre, fre.t)
